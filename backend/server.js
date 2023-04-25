1500ac6ad746a1cbeda990682c6c06683eccecdb
Dhamodhar <56181018+Dhamodhar-DDR@users.noreply.github.com>
2023-04-25 16:33:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 // Add middleware for handling sessions
