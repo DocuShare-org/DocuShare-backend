5c7866f600c0752c47153cbe0f1943c80eb28650
Dhamodhar <56181018+Dhamodhar-DDR@users.noreply.github.com>
2023-04-26 12:42:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,11 @@ const corsOptions ={
     optionSuccessStatus:200
 }
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 
 // Add middleware for handling sessions
