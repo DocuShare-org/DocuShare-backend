7535a7dd36da3637bdcfe3f23bebb7ab999b353b
Dhamodhar-DDR <dhamodhar.reddy@iiitb.org>
2023-05-12 19:35:33 +0530

    mounted a volume to store log data

--- backend/log.js
+++ backend/log.js
@@ -26,7 +26,7 @@ function logtofile(text, file = 'default.log', delimiter = '\n') {
 }
 
 const flog = (str) => {
-	logtofile(str, process.env.LOG_FILE_NAME || "backend.log");
+	logtofile(str, process.env.LOG_FILE_NAME || "../logs/backend.log");
 };
 
 module.exports = flog;
